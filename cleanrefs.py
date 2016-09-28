b60d8c52e431b0edc9e8df89d7c93401b1944788
Ali Alkhatib <ali.alkhatib@cs.stanford.edu>
2016-09-27 17:11:50 -0700

    cleaned refs

--- cleanrefs.py
+++ cleanrefs.py
@@ -2,9 +2,12 @@ import re,itertools
 with open('references.bib') as references:
   refs=references.read()
 
-refObjects=re.findall("(@(?:.*?)\})\n", refs,re.MULTILINE|re.DOTALL)
+refObjects=re.findall("(@(?:.*?)\}\n\})\n", refs,re.MULTILINE|re.DOTALL)
 print("Found {} references...".format(len(refObjects)))
 
+
+# print(refObjects[0])
+# quit()
 bibliography=list()
 longest_field=0
 
@@ -20,7 +23,7 @@ for obj in fields:
   temp=set(itertools.chain(*filter(None,map(fieldName.findall,obj))))
   results=results.union(temp)
 # print(results)
-longest_field=len(max(results,key=lambda n:len(n)))
+longest_field=max(results,key=lambda n:len(n))
 
 # print(longest_field)
 # quit()
@@ -29,10 +32,16 @@ for ref in refObjects:
   for line in ref.split("\n"):
     output=list()
     result=re.findall("^\s*(\S+)\s*=\s*\{(.*)\}(?:,|\})",line)
-    output.append("  {field:{width}} = {{{entry}}},".format(field=result[0][0],width=longest_field,entry=result[0][1]) if result else line or "}")
+    # if result and result[0][0]=="year":
+    #   print(result)
+    #   quit()
+    output.append("  {field}{space} = {{{entry}}},".format(field=result[0][0],
+                                                           space=" "*(len(longest_field)+15-len(result[0][0])),
+                                                           entry=result[0][1]) if result else line or "}")
     bibliography.append(output[0])
-    # print("\n".join(output))
+    print("\n".join(output))
 
-print("\n".join(bibliography))
-# for n in bibliography:
-#   print(n)
+
+# print("\n".join(bibliography))
+for n in bibliography:
+  print(n)
